bf3c89c84565fc6448705b3ffd0eadac63e8d17b
Anthony_Muscarella <muscarellaanthony1@gmail.com>
2024-04-13 15:03:37 -0400

    added css reset began building html

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,31 @@ const weatherAPIKey = '511954d1c4cc056059eabc45e69ef6f6'
 const city = 'london'
 const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPIKey}`;
 
-const weatherSearch = function(){
+// function to call api and retrieve data
+const weatherSearch = function () {
+    // fetch api
     fetch(queryURL)
-    .then (function (response){
-        if (response.ok){
-            response.json()
-            .then (function (data){
-                console.log(data)
-            })
-        } else {
-            alert(`Error:${response.statusText}`);
-          }
-    })
+        .then(function (response) {
+            // check to see the respons returned successfully
+            if (response.ok) {
+                //parse the data
+                response.json()
+                    .then(function (data) {
+                        console.log(data)
+                        // send the date to the display function
+                        displayData(data)
+                    })
+            } else {
+                // send error alers if response failed
+                alert(`Error:${response.statusText}`);
+            }
+        })
 }
+
+const displayData = function (data){
+    
+}
+
+
 
 weatherSearch()
